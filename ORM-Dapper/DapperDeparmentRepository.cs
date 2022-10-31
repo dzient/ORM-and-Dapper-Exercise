aa2caa469f01d0d213107ad15e3272b4e269970c
dzient <34776686+dzient@users.noreply.github.com>
2022-10-31 13:36:06 -0400

    Add files via upload
    First commit (Exercise 1)

---
+++ ORM-Dapper/DapperDeparmentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    internal class DapperDeparmentRepository
+    {
+        public IEnumerable? GetAllDepartments()
+        {
+            return null;
+        }
+        public void InsertDepartment()
+        {
+            Department department = new Department();
+        }
+    }
+}
